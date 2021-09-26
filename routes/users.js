eb4d8a23c2d17cbac5724c7516396c175214a831
kabiwabi <91104747+kabiwabi@users.noreply.github.com>
2021-09-26 11:34:03 -0400

    added mock database as a array for test

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,17 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    }
+]
+
 // all routes already start with '/users'
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
+    res.send(users);
     res.send('Hello');
 });
 
