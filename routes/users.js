f8259b417622d9e1a80df5eae0d7f0d9923279b0
kabiwabi <91104747+kabiwabi@users.noreply.github.com>
2021-09-26 12:47:58 -0400

    added patch function

--- routes/users.js
+++ routes/users.js
@@ -4,18 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-let users = [
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: 25
-    },
-    {
-        firstName: "Jane",
-        lastName: "Doe",
-        age: 25
-    }
-]
+let users = []
 
 // all routes already start with '/users'
 router.get('/', (req, res) => {
@@ -44,6 +33,19 @@ router.delete('/:id', (req,res) => {
     users = users.filter((user) => user.id !== id);
 
     res.send(`User with the id ${id} deleted from the database`);
-})
+});
+
+router.patch('/:id', (req,res) => {
+    const { id } = req.params;
+    const { firstName, lastName, age} = req.body;
+
+    const user = users.find((user) => user.id === id);
+
+    if (firstName) user.firstName = firstName;
+    if (lastName) user.lastName = lastName;
+    if (age) user.age = age;
+
+    res.send(`User with the id ${id} has been updated`);
+});
 
 export default router;
